c020a34d8b4e5c02cb997f8cd20e4dec834232ea
Robin <robin.skaba@volny.cz>
2024-01-06 16:12:55 +0100

    reworked link args

---
+++ app.py
@@ -0,0 +1,5 @@
+from server import app
+
+if __name__ == "__main__":
+    print("Server is online.")
+    app.run()
--- main.py
+++
@@ -1,5 +0,0 @@
-from server import app
-
-if __name__ == "__main__":
-    print("Server is online.")
-    app.run(host='0.0.0.0', port=9100)
